fe28ee48b4b6edf088af766eb772e4e0c2b3643a
naooaf <nayfserag5@gmail.com>
2023-12-13 22:18:25 +0200

    create hook to fetch games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,26 +1,6 @@
-import  { useEffect, useState } from 'react'
-import apiClient from '../services/api-client'
-interface Game {
-    id: number
-    name: string
-}
-interface FetchGameRes {
-    count: number
-    results : Game[]
-}
-
+import useGames from "../hooks/useGames"
 export const GameGrid = () => {
-    const [games, setGames] = useState<Game[]>([])
-    const [error , setError] = useState('')
-    useEffect(() => {
-        apiClient.get<FetchGameRes>('/games')
-        .then(res => {
-            setGames(res.data.results)
-        })
-        .catch(err => {
-            setError(err.message)
-        })
-    })
+    const {games, error} = useGames()
   return (
     <ul>
         {error && <li>{error}</li>}
